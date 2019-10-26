e7568d3d38e30ed869f90ff60bd1d7cc77cd2d59
espenbrekke <brekke.espen@gmail.com>
2019-10-26 16:47:20 +0200

    Update print.py

--- build/print.py
+++ build/print.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
@@ -11,4 +12,13 @@ while i < len(sys.argv):
       print(arg+" exists")
     else: 
       print(arg+" missing")
+    i += 1
+
+i = 1
+while i < len(sys.argv):
+    arg=sys.argv[i]
+    if os.path.isfile(arg):
+      with open('path_to_file/person.json') as f:
+        data = json.load(f)
+      print(data)
     i += 1
