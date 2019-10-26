f7ba1de5f39326a2c535e9688fb316f75fbcd691
espenbrekke <brekke.espen@gmail.com>
2019-10-26 16:07:37 +0200

    Update print.py

--- build/print.py
+++ build/print.py
@@ -1,4 +1,14 @@
 import sys
+import os
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
+
+i = 1
+while i < len(sys.argv):
+    arg=sys.argv[i]
+    os.path.isfile(arg)
+      print(arg+" exists")
+    else 
+      print(arg+" missing")
+    i += 1
