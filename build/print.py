f336dfe2d2eaf73f8c4cad7298fc0bd6df7a3388
espenbrekke <brekke.espen@gmail.com>
2019-10-26 20:28:15 +0200

    Update print.py

--- build/print.py
+++ build/print.py
@@ -21,6 +21,7 @@ def populateIndex(data):
         item=data[key]
         if isinstance(item, list):
             populateIndexFromList(item)
+        elif isinstance(item,string):
         elif isinstance(item,dict):
             if "id" in item:
                 id=item["id"]
