c0838441133bfad8f1ac5cb9daa7d851f1d6824b
espenbrekke <brekke.espen@gmail.com>
2019-10-26 21:13:05 +0200

    Update print.py

--- build/print.py
+++ build/print.py
@@ -5,15 +5,6 @@ import json
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
 
-i = 1
-while i < len(sys.argv):
-    arg=sys.argv[i]
-    if os.path.isfile(arg):
-      print(arg+" exists")
-    else: 
-      print(arg+" missing")
-    i += 1
-
 index={}
    
 def populateIndex(data):
