c3c4fa8242dc028b0e6bbafc8cb834b360dcdc96
espenbrekke <brekke.espen@gmail.com>
2019-10-27 18:59:00 +0100

    Update print.py

--- build/print.py
+++ build/print.py
@@ -2,8 +2,8 @@ import sys
 import os
 import json
 
-print 'Number of arguments:'+ len(sys.argv)+ 'arguments.'
-print 'Argument List:'+ str(sys.argv)
+print("Number of arguments:"+ len(sys.argv)+ "arguments.")
+print("Argument List:"+ str(sys.argv))
 
 index={}
    
